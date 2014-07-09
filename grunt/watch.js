b55231cb1bed8938b1676675d4bc079642b5f422
Daniel Tijerina <Daniel.Tijerina@springbox.com>
2014-07-09 17:12:53 -0500

    watcher

---
+++ grunt/watch.js
@@ -0,0 +1,64 @@
+module.exports = {
+    gruntfile: {
+        files: ['Gruntfile.js']
+    },
+
+    html: {
+         files: [ '<%= paths.src %>/*.html' ],
+         tasks: ['htmlmin:dist'],
+         options: {
+            livereload: true
+        }
+    },
+
+    sass: {
+        files: [ '<%= paths.src %>/styles/**/*.scss' ],
+        tasks: ['sass', 'autoprefixer', 'csso'],
+        options: {
+            livereload: false
+        }
+    },
+
+    css: {
+        files: [ 
+            '<%= paths.dist %>/styles/**/*.css',
+            '!<%= paths.dist %>/styles/**/*.min.css'
+        ],
+        options: {
+            livereload: true
+        }
+    },
+
+    images: {
+        files: [ '<%= paths.src %>/images/**/*.{gif,jpeg,jpg}' ],
+        tasks: [ 'newer:imagemin'],
+        options: {
+            livereload: false
+        }
+    },
+
+    pngs: {
+        files: [ '<%= paths.src %>/images/**/*.png' ],
+        tasks: [ 'newer:pngmin'],
+        options: {
+            livereload: false
+        }
+    },
+
+    optimizedImages: {
+        files: [ '<%= paths.dist %>/images/**/*.{gif,jpeg,jpg,png}' ],
+        options: {
+            livereload: true
+        }
+    },
+
+
+    scripts: {
+        files: [ '<%= paths.src %>/scripts/**/*.js' ],
+        tasks: [ 'uglify' ],
+        options: {
+            livereload: true
+        }
+    },
+    
+}
