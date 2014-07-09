66df536da59fda6c78a60ff0497cb55da54e9357
Daniel Tijerina <Daniel.Tijerina@springbox.com>
2014-07-09 17:13:00 -0500

    static files and SASS

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 //create the server
 http.listen(8080);
 
-//Application Routing
-app.get('/', function(req, res){
-  res.sendfile('src/index.html');
-});
+//set up the static resources (HTML, JS, CSS)
+app.use(express.static(__dirname + '/dist/'));
 
-
+//SOCKETS!!!
 io.on('connection', function(socket){
   console.log('a user connected');
 });
