760019fe235a278851a307821292487149f27ed3
Daniel Tijerina <Daniel.Tijerina@springbox.com>
2014-07-09 15:39:58 -0500

    base socket app

---
+++ index.js
@@ -0,0 +1,17 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+//create the server
+http.listen(8080);
+
+//Application Routing
+app.get('/', function(req, res){
+  res.sendfile('app/index.html');
+});
+
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
