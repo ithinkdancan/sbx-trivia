c86374f35037a9e68e01fc41a2c52ad19fc10842
Daniel Tijerina <Daniel.Tijerina@springbox.com>
2014-08-07 10:00:15 -0500

    fake users

--- index.js
+++ index.js
@@ -236,9 +236,9 @@ io.on('connection', function(socket){
 
 createGame();
 
-// for (var i = 0; i < 100; i++) {
-// 	var user = new User('user-' + i);
-// 	user.avatar = 'http://lorempixel.com/200/200/?id='+users.length;
-// 	users.push(user);
-// };
+for (var i = 0; i < 100; i++) {
+	var user = new User('user-' + i);
+	user.avatar = 'http://lorempixel.com/200/200/?id='+users.length;
+	users.push(user);
+};
 
